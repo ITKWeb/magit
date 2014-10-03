1d3e2d5576b843e82d5e77fc3fda9f32a73b7202
MarineMiquet <marine.miquet@itkweb.com>
2014-10-03 16:11:10 +0200

    noeud de depart et arrivee enregistree au clic, ligne moche

--- app/scripts/directives/TreeGit.js
+++ app/scripts/directives/TreeGit.js
@@ -12,17 +12,23 @@ angular.module('magitApp').directive('treeGit', [
                 var margin =  20;
                 var height =  900;
                 var width = 800;
+                var svg, dragLine, root, isDragging, fromNode, toNode, source, target;
 
                 var draw = function draw(dataTree) {
-                 
                     element[0].innerHTML = '';
+
                     var i = 0;
+                    isDragging = false;
+                    fromNode=null;
+                    toNode=null;
                     var tree = d3.layout.tree().size([height, width]);
                     
                     var diagonal = d3.svg.diagonal()
                         .projection(function(d) { return [d.x, d.y]; }); //invert x and y for horizontal
-                     
-                    var svg = d3.select(element[0])
+
+                    
+                    
+                    svg = d3.select(element[0])
                         .append('svg')
                         .attr('width', width)
                         .attr('height',  height)
@@ -30,13 +36,86 @@ angular.module('magitApp').directive('treeGit', [
                         .append('g')
                     .attr('transform', 'translate(0 ,' + margin + ')');
                     
-                    var root = dataTree;
-                    update(svg,root, tree,diagonal, i);
+                    root = dataTree;
+                    update(svg,tree,diagonal, i);
+                    
+                    
+                    var lineLayer = svg.append('g');
+                     
+                    dragLine = lineLayer.append('line')
+                      .attr('class', 'dragLine');
+                    
+                    
+                    svg.selectAll('.node')
+                        .on('mousedown', function (d) {
+                            if(fromNode === null){
+                                fromNode = d;
+                                dragLine
+                                    .style('class', 'dragLine')
+                                    .attr({
+                                        x1: d.x,
+                                        y1: d.y,
+                                        x2: d.x,
+                                        y2: d.y
+                                    });
+                                toNode=null;
+                                isDragging = true;
+                            }else {
+                            
+                                //NO click to self
+                                if(fromNode !== d){
+                                    toNode = d;
+                                    dragLine
+                                        .style('class', 'dragLine')
+                                        .attr({
+                                            x1: fromNode.x,
+                                            y1: fromNode.y,
+                                            x2: d.x,
+                                            y2: d.y
+                                        });
+                                    isDragging=false;
+                                }
+                            }                    
+                        })
+                        .on('mouseup', function () {
+                           if(fromNode !== null && toNode !== null && !isDragging){
+                               source = fromNode;
+                               target = toNode;
+                               fromNode=null;
+                               toNode=null;
+                               console.log('Je vais de là:', source, ' à de là: ', target);
+                               
+                               isDragging=false;
+                           }
+                        })
+                        .on('mouseover', function () {
+                           d3.select(this).attr('z-index', 10)
+                            .attr('class', 'hoverNode');
+                        })
+                        .on('mouseout', function () {
+                           d3.select(this).attr('z-index', 1)
+                            .attr('class', 'node');
+                        });                  
+       
+                    svg.on('mousemove', function () {
+                        if(fromNode !== null)
+                        {
+                            var m = d3.mouse(this);
+                            dragLine.attr({
+                                            x1: fromNode.x,
+                                            y1: fromNode.y,
+                                            x2: m[0]-1,
+                                            y2: m[1]-2
+                                        });                     
+                        }
+
+                    });
                      
                 };
 
-                
-                var update = function update(svg,root,tree,diagonal,i) {
+ 
+                //Draw all the tree
+                 var update = function update(svg,tree,diagonal,i) {
 
                     // Compute the new tree layout.
                     var nodes = tree.nodes(root).reverse(),
@@ -52,6 +131,7 @@ angular.module('magitApp').directive('treeGit', [
                     // Enter the nodes.
                     var nodeEnter = node.enter().append('g')
                         .attr('class', 'node')
+                       /* .call(dragListener)*/
                         .attr('transform', function(d) {
                             return 'translate(' + d.x + ',' + d.y + ')'; }); //invert x and y for horizontal
 
@@ -79,6 +159,7 @@ angular.module('magitApp').directive('treeGit', [
 
                 };
                 
+
                 scope.$watch('dataTree', function(newValue) {
                     if(newValue !== undefined) {
                         draw(scope.dataTree);
