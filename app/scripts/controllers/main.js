e6ddc5d4066cbcb2c0eeee12f554369d4cfbcf8e
MarineMiquet <marine.miquet@itkweb.com>
2014-10-03 13:00:14 +0200

    fake arbre et style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,8 +12,32 @@ angular.module('magitApp')
     function ($scope) {
       
         var displayCharts = function displayCharts() {
-            $scope.dataTree = 'coucou'; //service.mesdata 
-            console.log('hey');
+            $scope.dataTree = [
+                  {
+                    'name': 'Top Level',
+                    'parent': 'null',
+                    'children': [
+                      {
+                        'name': 'Level 2: A',
+                        'parent': 'Top Level',
+                        'children': [
+                          {
+                            'name': 'Son of A',
+                            'parent': 'Level 2: A'
+                          },
+                          {
+                            'name': 'Daughter of A',
+                            'parent': 'Level 2: A'
+                          }
+                        ]
+                      },
+                      {
+                        'name': 'Level 2: B',
+                        'parent': 'Top Level'
+                      }
+                    ]
+                  }
+                ];
         };
       
 
--- app/scripts/directives/TreeGit.js
+++ app/scripts/directives/TreeGit.js
@@ -16,22 +16,70 @@ angular.module('magitApp').directive('treeGit', [
                 var draw = function draw(dataTree) {
                  
                     console.log(element[0],'draw',dataTree); 
+                    var i = 0;
+                     var tree = d3.layout.tree().size([height, width]);
+                    
+                    var diagonal = d3.svg.diagonal()
+                        .projection(function(d) { return [d.y, d.x]; });
+                     
                     var svg = d3.select(element[0])
                         .append('svg')
                         .attr('width', width)
                         .attr('height',  height)
-                        .attr('viewBox', '0 0 ' + width + ' ' + height);
+                        .attr('viewBox', '0 0 ' + width + ' ' + height)
+                        .append('g');
                     
-                    svg.append('rect')
-                    .attr('width', width + 'px')
-                    .attr('height', height + 'px')
-                    .attr('fill', 'rgba(200, 255, 111, 0.52)');
+                    var root = dataTree[0];
+                    update(svg,root, tree,diagonal, i);
                      
                 };
 
+                
+                var update = function update(svg,root,tree,diagonal,i) {
+
+                    // Compute the new tree layout.
+                    var nodes = tree.nodes(root).reverse(),
+                    links = tree.links(nodes);
+
+                    // Normalize for fixed-depth.
+                    nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+                    // Declare the nodes…
+                    var node = svg.selectAll('g.node')
+                        .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+                    // Enter the nodes.
+                    var nodeEnter = node.enter().append('g')
+                        .attr('class', 'node')
+                        .attr('transform', function(d) {
+                            return 'translate(' + d.y + ',' + d.x + ')'; });
+
+                    nodeEnter.append('circle')
+                        .attr('r', 10)
+                        .style('fill', '#fff');
+
+                    nodeEnter.append('text')
+                        .attr('x', function(d) {
+                            return d.children || d._children ? -13 : 13; })
+                        .attr('dy', '.35em')
+                        .attr('text-anchor', function(d) {
+                            return d.children || d._children ? 'end' : 'start'; })
+                        .text(function(d) { return d.name; })
+                        .style('fill-opacity', 1);
+
+                    // Declare the links…
+                    var link = svg.selectAll('path.link')
+                        .data(links, function(d) { return d.target.id; });
+
+                    // Enter the links.
+                    link.enter().insert('path', 'g')
+                        .attr('class', 'link')
+                        .attr('d', diagonal);
+
+                };
+                
                
                 draw(scope.dataTree);
-                 console.log('hey2');
                 
             }
         };
