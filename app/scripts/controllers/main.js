46127eefe0d99d96e3208086a8ae65b29bd83040
Romain Maneschi <romain.maneschi@itkweb.com>
2014-10-03 10:28:18 +0200

    clean generated files + put first DOM architecture of project

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('magitApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.gitLgResText = '';
   });
