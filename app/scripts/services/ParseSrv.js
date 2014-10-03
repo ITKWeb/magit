2d16747ba88f8905257e5889748fa4fe70cdf506
Romain Maneschi <romain.maneschi@itkweb.com>
2014-10-03 14:30:01 +0200

    finish parsing of complexe git tree

--- app/scripts/services/ParseSrv.js
+++ app/scripts/services/ParseSrv.js
@@ -21,6 +21,7 @@ angular.module('magitApp').factory('ParseSrv', function () {
       comment: comment,
       time: time,
       user: user,
+      parents: parents,
       tags: tags,
       branchs: banchs,
       children: children
@@ -31,13 +32,13 @@ angular.module('magitApp').factory('ParseSrv', function () {
     var res = line.match(regexpLine);
     if(res !== null) {
       var hash = res[1];
-      var parentHashs = res[2];
+      var parentHashs = res[2] !== undefined ? res[2].split(' ') : [];
       var time = res[3];
       var branchs = res[4];
       var user = res[5];
       var comment = res[6];
       if(branchs === undefined) {//no branch
-        return buildCommit(hash, comment, time, user);
+        return buildCommit(hash, comment, time, user, parentHashs);
       } else {
         branchs = branchs.replace(/(\s|\(|\))/g, '').split(',');
         return buildCommit(res[1], res[4], res[5], res[6], parentHashs, branchs);
@@ -45,28 +46,38 @@ angular.module('magitApp').factory('ParseSrv', function () {
     }
   };
 
-  var reorganizeCommit = function reorganizeCommit(commits) {
-    var commitInit = commits[0];
-    for(var i=1, len=commits.length; i<len; i++) {
-      commitInit.children.push(commits[i]);
-      commitInit = commits[i];
+  var reorganizeCommit = function reorganizeCommit(commits, commitsByHash) {
+    var firstNode = commits[0];
+    for(var i=0, len=commits.length; i<len; i++) {
+      var commitCurrent = commits[i];
+      var parentsCommits = commitCurrent.parents;
+      for(var j=0, lenJ = parentsCommits.length; j<lenJ; j++) {
+        var parentCommit = commitsByHash[parentsCommits[j]];
+        if(parentCommit !== undefined) {
+          parentCommit.children.push(commitCurrent);
+        } else {//user hasn't give all the tree
+          firstNode = commitCurrent;
+        }
+      }
     }
-    return commits[0];
+    return firstNode;
   };
 
   return {
     parse: function(gitLgResText) {
       var lines = gitLgResText.split('\n');
       var commits = [];
+      var commitsByHash = {/*hash: Commit*/};
       for(var i=0, len=lines.length; i<len; i++) {
         var commit = parseLine(lines[i]);
         if(commit !== undefined) {
+          commitsByHash[commit.hash] = commit;
           commits.push(commit);
         } else {
           console.error('not parsed', lines[i]);
         }
       }
-      return reorganizeCommit(commits.reverse());
+      return reorganizeCommit(commits.reverse(), commitsByHash);
     }
   };
     
--- test/spec/services/ParseSrv.js
+++ test/spec/services/ParseSrv.js
@@ -34,4 +34,25 @@ describe('Services: ParseSrv', function () {
     expect(res.children[0].children[0].children[0].branchs[0]).toBe(DataMockedSrv.getSimpleDataMocked().children[0].children[0].children[0].branchs[0]);
   }));
 
+  it('should parse a complexe tree', inject( function (ParseSrv, DataMockedSrv) {
+    var stringToParse = [
+      '731cd4a#bc55f20#il y a 31 minutes| (HEAD, master)|Romain Maneschi|change git commande and parse implementation in 10 minutes thanks to tdd :D',
+      'bc55f20#d99ed02#il y a 31 minutes||Romain Maneschi|fake commit to simulate multiple branch dijonction',
+      'd99ed02#d642469 daaf2d7#il y a 53 minutes| (origin/master, origin/HEAD)|MarineMiquet|Merge branch \'treeGit_directive\'',
+      'daaf2d7#e6ddc5d#il y a 54 minutes||MarineMiquet|dataTree vertical OK avec donnees mockees',
+      'e6ddc5d#81f4ded#il y a 74 minutes||MarineMiquet|fake arbre et style',
+      '81f4ded#4f8f953#il y a 74 minutes||MarineMiquet|fichiers de conf OK et bon nom de directive',
+      '4f8f953#4162e5f#il y a 75 minutes||MarineMiquet|coucoutest',
+      '4162e5f#d642469#il y a 75 minutes||MarineMiquet|directive cree',
+      '872a020#d642469#il y a 83 minutes| (complexeTree)|Romain Maneschi|fake commit for multiple branchs',
+      'd642469#1218574#il y a 89 minutes||Romain Maneschi|parse simple branch'
+    ].join('\n');
+    var res = ParseSrv.parse(stringToParse);
+    expect(res.comment).toBe('parse simple branch');
+    expect(res.children.length).toBe(3);
+    expect(res.children[0].hash).toBe('872a020');
+    expect(res.children[1].hash).toBe('4162e5f');
+    expect(res.children[2].hash).toBe('d99ed02');
+  }));
+
 });
